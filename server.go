d9fe0ef5641c893998ea6a426849823efde15b99
glaslos <lukas_rist@symantec.com>
2017-06-23 13:44:33 +0200

    dugnad

--- server.go
+++ server.go
@@ -16,11 +16,10 @@ var l = ttlru.New(128, 20*time.Second)
 
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
-	fmt.Fprintf(w, "Hi tester %q\n", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hi %q\n", html.EscapeString(r.URL.Path))
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
 	ret := false
 	key := r.URL.Query().Get("key")
 	value := r.URL.Query().Get("value")
@@ -30,7 +29,11 @@ func api(w http.ResponseWriter, r *http.Request) {
 		ret = l.Set(key, value)
 	} else if r.Method == "GET" {
 		inter, _ := l.Get(key)
-		value, _ = inter.(string)
+		if value, ret = inter.(string); ret {
+			/* act on str */
+		} else {
+			/* not string */
+		}
 	}
 	fmt.Fprintf(w, "%q, %q", value, strconv.FormatBool(ret))
 }
@@ -42,5 +45,6 @@ func main() {
 	http2.ConfigureServer(&srv, &http2.Server{})
 	// openssl genrsa -out server.key 2048
 	// openssl req -new -x509 -key server.key -out server.pem -days 3650
+	// testing: h2load -n10000 -c100 -m10 "https://localhost/api?key=foo"
 	log.Fatal(srv.ListenAndServeTLS("server.pem", "server.key"))
 }
